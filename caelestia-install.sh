53e0483ec9465c5ca1a874c2d34d7855dd6d046d
Shadoxu <shadoxu.developer.arc+github.com@gmail.com>
2026-04-17 17:44:14 +0530

    Update caelestia-install.sh with new content

--- caelestia-install.sh
+++ caelestia-install.sh
@@ -2,23 +2,23 @@
 
 # Updated script with variable expansions fixed
 
-LOCAL_BIN="~/.local/bin"
+LOCAL_BIN="${HOME}/.local/bin"
 
 # Some code... 
 
-if ! grep -q "\${LOCAL_BIN}" "${HOME}/.config/fish/config.fish" 2>/dev/null; then
+if ! grep -q "${LOCAL_BIN}" "${HOME}/.config/fish/config.fish" 2>/dev/null; then
     # Do something
 fi
 
 # Some more code... 
 
-if ! grep -q "\${LOCAL_BIN}" "${HOME}/.bashrc" 2>/dev/null; then
+if ! grep -q "${LOCAL_BIN}" "${HOME}/.bashrc" 2>/dev/null; then
     # Do something
 fi
 
 # Some other code... 
 
-if ! grep -q "\${LOCAL_BIN}" "${HOME}/.zshrc" 2>/dev/null; then
+if ! grep -q "${LOCAL_BIN}" "${HOME}/.zshrc" 2>/dev/null; then
     # Do something
 fi
 
